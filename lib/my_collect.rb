3b045555171ad9c0fc282b25525ce49e0e85f053
grant smith <grantsmith1256@gmail.com>
2018-10-17 21:17:33 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,9 +1,18 @@
 def my_collect(array)
    new_array=[]
    i=0
+<<<<<<< HEAD
    while i < array.length
    new_array << yield(array[i])
    i+=1
  end
  new_array
+=======
+
+while i < array.length
+yield array[i]
+i+=1
+end
+new_array << my_collect(array)
+>>>>>>> 8b8bdf4d628f60389be4af274cc7a40aa682b9d7
 end
